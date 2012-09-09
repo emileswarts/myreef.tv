1f9f621867544fd8807fe30145bd6cebd7cbe38d
Emile Swarts <emile.swarts123@gmail.com>
2012-09-09 15:40:03 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myreef::Application.config.secret_token = 'f28b30378f37d53d6a9e937823d87c511ec77a816be7126166316eb624681fd00d5efd20c484f9d1c821c64a4b9c283d74fda7127b2ff0dc7732e51df9873f09'
