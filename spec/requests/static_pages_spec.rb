f4bb62b42aa90bbea644931387cf2e47639a97c6
Emile Swarts <emile.swarts123@gmail.com>
2012-09-10 09:04:36 +0100

    Acceptance test for help page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,13 +3,17 @@ require 'spec_helper'
 describe "Static Pages" do
 
   describe "Home page" do
-
-    it "It should have the content 'Sample app'" do
+    it "Should have the content 'Sample app'" do
 		visit '/static_pages/home'
 		page.should have_content('Sample App')
-
     end
+  end
 
+  describe "Help page" do
+	  it "Should have the content 'Help'" do
+		  visit '/static_pages/help'
+		  page.should have_content('Help')
+	  end
   end
 
 end
