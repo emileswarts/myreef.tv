f37f2a2335cdf71de730772729923fe3979a0ad4
Emile Swarts <emile.swarts123@gmail.com>
2012-09-10 21:52:24 +0100

    static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,9 +3,14 @@ require 'spec_helper'
 describe "Static Pages" do
 
   describe "Home page" do
-    it "Should have the content 'Sample app'" do
+
+	  it "Should have title h1 'Myreef.tv'" do
+		  visit '/static_pages/home'
+		  page.should have_selector('h1', :text => 'Myreef.tv')
+	  end
+    it "Should have the content 'Sign in'" do
 		visit '/static_pages/home'
-		page.should have_content('Sample App')
+		page.should have_content('Sign in')
     end
   end
 
@@ -13,6 +18,14 @@ describe "Static Pages" do
 	  it "Should have the content 'Help'" do
 		  visit '/static_pages/help'
 		  page.should have_content('Help')
+	  end
+  end
+
+  describe "About page" do
+	  it "Should have content 'About us'" do
+		  visit '/static_pages/about'
+		  page.should have_content('About')
+
 	  end
   end
 
