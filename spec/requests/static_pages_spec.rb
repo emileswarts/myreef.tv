57b1acf8044d9f1a4b93781b6f588f6fa640be9f
Emile Swarts <emile.swarts123@gmail.com>
2012-09-12 20:55:02 +0100

    tset

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,22 +2,59 @@ require 'spec_helper'
 
 describe "Static Pages" do
 
-  describe "Home page" do
+	describe "Home page" do
 
-	  it "Should have h1 header 'Myreef.tv'" do
-		  visit '/static_pages/home'
-		  page.should have_selector('h1', :text => 'Myreef.tv')
-	  end
+		it "Should have title h1 'Myreef.tv'" do
+			visit '/static_pages/home'
+			page.should have_selector('h1', :text => 'Myreef.tv')
+		end
 
-	  it "Should have the base title" do
-		  visit '/static_pages/home'
-		  page.should have_selector('h1', 
-									:text => 'Myreef.tv')
-	  end
 
-	  it "It should not have a custom page title" do
-		  visit '/static_pages/home'
-		  page.should_not have_selector('h1', :text => '| Home')
-	  end
-  end
+		it "Should have h1 header 'Myreef.tv'" do
+			visit '/static_pages/home'
+			page.should have_selector('h1', :text => 'Myreef.tv')
+		end
+
+		it "Should have the base title" do
+			visit '/static_pages/home'
+			page.should have_selector('h1', :text => 'Myreef.tv')
+		end
+
+		it "It should not have a custom page title" do
+			visit '/static_pages/home'
+			page.should_not have_selector('h1', :text => '| Home')
+		end
+
+		it "Should have the title 'Home'" do
+			visit '/static_pages/home'
+			page.should have_selector('title', :text => 'Myreef.tv')
+		end
+
+	end
+
+	describe "Help page" do
+
+		it "Should have title h1 'Help'" do
+			visit '/static_pages/help'
+			page.should have_selector('h1', :text => 'Help')
+		end
+		it "Should have the title 'Help'" do
+			visit '/static_pages/help'
+			page.should have_selector('title', :text => 'Help')
+		end
+
+	end
+
+	describe "About page" do
+
+		it "Should have title h1 'About us'" do
+			visit '/static_pages/about'
+			page.should have_selector('h1', :text => 'About us')
+		end
+		it "Should have the title 'About us'" do
+			visit '/static_pages/about'
+			page.should have_selector('title', :text => 'About')
+		end
+
+	end
 end
