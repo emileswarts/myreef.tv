b3c4aa89650e8512e3a18c9a1d8f9f978624ed73
Emile Swarts <emile.swarts123@gmail.com>
2012-09-14 08:53:38 +0100

    validate presence name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,22 @@
 require 'spec_helper'
 
 describe User do
-	before { @user = User.new(name: "Example user", email: "user@example.com") }
+
+	before do 
+		@user = User.new(name: "Example user", email: "user@example.com") 
+	end
+
 	subject{@user}
 
 	it { should respond_to(:name)}
 	it { should respond_to(:email)}
+
+	it { should be_valid }
+
+	describe "When a name is not present" do
+		before { @user.name = " " }
+		it { should_not be_valid }
+	end
 end
 # == Schema Information
 #
