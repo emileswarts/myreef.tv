4b6233d633134420d055d1c516391dff31976aa6
Emile Swarts <emile.swarts123@gmail.com>
2012-09-14 08:59:26 +0100

    More user model validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,6 +17,17 @@ describe User do
 		before { @user.name = " " }
 		it { should_not be_valid }
 	end
+
+	describe "When an email is not present" do
+		before {@user.email = ""}
+		it { should_not be_valid }
+	end
+
+	describe "When a name is too long" do
+		before { @user.name = "a" * 51 }
+		it { should_not be_valid }
+	end
+	
 end
 # == Schema Information
 #
