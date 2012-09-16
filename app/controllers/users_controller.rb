3d362817084fbe2408b6fe7a746bded1c96a1749
Emile Swarts <emile.swarts123@gmail.com>
2012-09-16 21:42:24 +0100

    edit users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,12 @@ class UsersController < ApplicationController
   end
 
   def update
+	  @user = User.find(params[:id])
+		if @user.update_attributes(params[:user])
+			#success
+		else
+			render 'edit'
+		end
   end
 
 end
