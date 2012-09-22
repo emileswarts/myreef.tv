3e7dda0d28ee68b2000263997a696266259c00da
Emile Swarts <emile.swarts123@gmail.com>
2012-09-22 01:43:05 +0100

    more gallery controller

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,4 +1,27 @@
 class GalleriesController < ApplicationController
+
 	def index
+	end
+
+	def edit
+	  @gallery = Gallery.find(params[:id])
+	end
+
+	def new
+	  @gallery = Gallery.new
+	end
+
+	def create
+		@gallery = current_user.galleries.build(params[:gallery])
+		if @gallery.save(params[:gallery] )
+			flash[:success] = "Item saved"
+			redirect_to @gallery
+		else
+			flash[:error] = "Could not save"
+		end
+	end
+
+	def show
+
 	end
 end
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,6 +1,11 @@
 class Gallery < ActiveRecord::Base
 
-  attr_accessible :title, :file
+  attr_accessible :title, :photo
+  has_attached_file :photo, :styles => { :medium => "800x800>", :thumb => "100x100>"}
+  attr_accessor :photo_file_name
+  attr_accessor :photo_content_type
+  attr_accessor :photo_file_size
+  attr_accessor :photo_updated_at
 
   belongs_to :user
 
