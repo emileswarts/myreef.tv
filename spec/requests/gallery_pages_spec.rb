16034abc44df412b4cc9d4f6174362e528d48771
Emile Swarts <emile.swarts123@gmail.com>
2012-10-10 19:06:17 +0100

    shared error messages

--- spec/requests/gallery_pages_spec.rb
+++ spec/requests/gallery_pages_spec.rb
@@ -1,7 +1,32 @@
 require 'spec_helper'
 
-describe "When visiting the gallery page" do
+describe "Gallery pages" do
+
 	subject { page }
-	before { visit galleries_path }
-	it { should have_selector('title', text: 'Gallery') }
+
+	let(:user) { FactoryGirl.create(:user) }
+	before { sign_in user }
+
+	describe "gallery creation" do
+#		before { visit root_path}
+#
+#		describe "with invalid information" do
+#			it "should not create a gallery image" do
+#				expect { click_button "submit" }.not_to change(Gallery, :count)
+#			end
+#
+#			describe "error messages" do
+#				before { click_button "submit" }
+#				it { should have_content("error") }
+#			end
+#		end
+#
+#		describe "with valid information" do
+#			before { fill_in 'gallery_title', with: "Test" }
+#			it "should create a gallery item" do
+#				expect { click_button "submit" }.to change(Gallery, :count).by(1)
+#			end
+#		end
+	end
+
 end
