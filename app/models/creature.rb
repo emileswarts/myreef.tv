2318b74f428ac532827a5f5e6b5d76d6f488ce4b
Emile Swarts <emile.swarts123@gmail.com>
2012-11-12 11:12:25 +0000

    Fix creature date

--- app/models/creature.rb
+++ app/models/creature.rb
@@ -2,7 +2,7 @@ class Creature < ActiveRecord::Base
 
   attr_accessible :date_aquired, :name, :rip_date, :scientific_name, :photo, :photo_file_name, :fishtank_id
 
-  has_attached_file :photo, :styles => { :medium => "800x800>", :thumb => "200x200#"}
+  has_attached_file :photo, :styles => { :medium => "800x800>", :thumb => "200x200#", :mini_thumb => "100x100"}
 
   validates :photo, presence: true
 
