e56a6e05951662db29da2208a02c3f754daa35ba
Emile Swarts <emile.swarts123@gmail.com>
2012-12-22 12:31:19 +0000

    add timeline featured to creatures

--- app/models/creature.rb
+++ app/models/creature.rb
@@ -1,6 +1,6 @@
 class Creature < ActiveRecord::Base
 
-		attr_accessible :date_aquired, :name, :rip_date, :scientific_name, :photo, :photo_file_name, :fishtank_id, :tag_list
+		attr_accessible :date_aquired, :name, :rip_date, :scientific_name, :photo, :photo_file_name, :fishtank_id, :tag_list, :timeline_featured
 
 		has_attached_file :photo, :styles => { :medium => "800x800>", :thumb => "200x200#", :mini_thumb => "100x100"}
 
