3496080c2c4fcb545457f4cf7e03bbcdda600774
Emile Swarts <emile.swarts123@gmail.com>
2012-12-23 15:24:48 +0000

    environment stuff

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Myreef::Application.initialize!
+
+test = [['Aggressive', 1], ['Semi-aggresive', 2], ['Peaceful', 3]]
