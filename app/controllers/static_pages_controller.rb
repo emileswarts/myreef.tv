9ffd052d6be13529124e41b695fc8a27daa34374
Emile Swarts <emile.swarts123@gmail.com>
2012-12-23 14:36:31 +0000

    limit timeline entries, homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,12 +10,11 @@ class StaticPagesController < ApplicationController
 			timeline = Timeline.new
 			@year_range = timeline.getTimelineYearRange(@featured_timeline_user) 
 
-			@timeline_creatures = Creature.where("timeline_featured = ?", 1)
+			@timeline_creatures = Creature.where("timeline_featured = ?", 1).limit(8)
 			@earliest_timeline_creature_timestamp = timeline.getStartDate(@timeline_creatures).to_time.to_i
 
 			#How long should each year <li> in the timeline be.  eg 2 will be 48% each (-2 for padding)
 			@year_range_count = 100 / @year_range.count - 2;
-
   end
 
   def about
